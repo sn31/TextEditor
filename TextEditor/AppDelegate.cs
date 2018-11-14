f2bc87ed8a2c949dbf55874355e4e3dfd6ff54a5
SkyeN <skye@dames.es>
2018-11-14 12:22:39 -0800

    add openDoc feature

--- TextEditor/AppDelegate.cs
+++ TextEditor/AppDelegate.cs
@@ -14,7 +14,24 @@ namespace TextEditor
         {
             // Insert code here to initialize your application
         }
+        [Export("openDocument:")]
+        void OpenDialog(NSObject sender)
+        {
+            var dlg = NSOpenPanel.OpenPanel;
+            dlg.CanChooseFiles = false;
+            dlg.CanChooseDirectories = true;
 
+            if (dlg.RunModal() == 1)
+            {
+                var alert = new NSAlert()
+                {
+                    AlertStyle = NSAlertStyle.Informational,
+                    InformativeText = "At this point we should do something with the folder that the user just selected in the Open File Dialog box...",
+                    MessageText = "Folder Selected"
+                };
+                alert.RunModal();
+            }
+        }
         public override void WillTerminate(NSNotification notification)
         {
             // Insert code here to tear down your application
